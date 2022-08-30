221a9e101880774d16836a5853bbb5255c0175ea
penyuss <111746650+penyuss@users.noreply.github.com>
2022-08-30 22:29:12 +0700

    Create railling.sh

---
+++ railling.sh
@@ -0,0 +1,9 @@
+You can add custom software and dependencies for your environment here. Some examples:
+
+RUN code-server --install-extension esbenp.prettier-vscode
+RUN sudo apt-get install -y build-essential
+RUN COPY myTool /home/coder/myTool
+
+Install NodeJS
+RUN sudo curl -fsSL https://deb.nodesource.com/setup_15.x | sudo bash -
+RUN sudo apt-get install -y nodejs
